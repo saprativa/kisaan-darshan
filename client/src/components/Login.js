8ab8fea1f32e5fc7ceb85adfee934bef68479a80
unknown <saprativa@gmail.com>
2020-07-26 20:40:10 +0530

    test

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
-class Login extends React.Component {
+class Login extends Component {
     render() {
       return (
         <div className="container">
-          Login
+          <Form>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input type="email" name="email" id="email" placeholder="Email" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input type="password" name="password" id="password" placeholder="Password" />
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
         </div>
-      );
+      )
     }
   }
   
-  export default Login;
+  export default Login
