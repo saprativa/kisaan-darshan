71035cd35f3d04458ef94bbc041b8195e66b2187
unknown <saprativa@gmail.com>
2020-07-30 11:12:25 +0530

    added registration page

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,13 +1,80 @@
-import React from 'react';
+import React from "react"
+import { useForm } from "react-hook-form"
+import { useHistory } from "react-router-dom";
+import { yupResolver } from '@hookform/resolvers'
+import * as yup from "yup"
+import axios from 'axios'
+import './Login.css'
 
-class Register extends React.Component {
-    render() {
-      return (
-        <div>
-          Register
-        </div>
-      );
-    }
-  }
+const schema = yup.object().shape({
+  firstName: yup.string().required("Please enter First Name."),
   
-  export default Register;
+})
+
+export default function Register() {
+
+  const { register, handleSubmit, errors, setError } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const history = useHistory();
+
+  const onSubmit = data => {
+    axios.post('/register', data)
+    .then((response) => {
+      console.log()
+      response.data.errors.forEach(element => {
+        setError(element.param, {type: "server", message: element.msg})
+      });
+      //setError(response.data.errors[0].param, {type: "server", message: response.data.errors[0].msg})
+      if (response.data.success) {
+       // history.push("/login")
+      } else {
+        //setError("server", {type: "manual", message: "Invalid Mobile Number/Password."})
+      }
+    })
+  }
+
+  return (
+     
+    <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+
+      <p className="error">{errors.server?.message}</p>
+      
+      <input type="text" name="firstName" placeholder="First Name" ref={register} />
+      <p className="error">{errors.firstName?.message}</p>
+
+      <input type="text" name="lastName" placeholder="Last Name" ref={register} />
+      <p className="error">{errors.lastName?.message}</p>
+
+      <input type="text" name="age" placeholder="Age" ref={register} />
+      <p className="error">{errors.age?.message}</p>
+
+      <input type="text" name="sex" placeholder="Sex" ref={register} />
+      <p className="error">{errors.sex?.message}</p>
+
+      <input type="text" name="mobile" placeholder="Mobile Number" ref={register} />
+      <p className="error">{errors.mobile?.message}</p>
+
+      <input type="text" name="email" placeholder="Email (optional)" ref={register} />
+      <p className="error">{errors.email?.message}</p>
+
+      <input type="text" name="village" placeholder="Village" ref={register} />
+      <p className="error">{errors.village?.message}</p>
+
+      <input type="text" name="block" placeholder="Block" ref={register} />
+      <p className="error">{errors.block?.message}</p>
+
+      <input type="text" name="district" placeholder="District" ref={register} />
+      <p className="error">{errors.district?.message}</p>
+
+      <input type="text" name="state" placeholder="State" ref={register} />
+      <p className="error">{errors.state?.message}</p>
+        
+      <input type="password" name="password" placeholder="Password" ref={register} />
+      <p className="error">{errors.password?.message}</p>
+      
+      <input type="submit" value="Register" />
+    </form>
+  )
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 var passport = require('passport')
 var Farmer = require('../models/farmer')
+const { body, validationResult } = require('express-validator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,5 +26,45 @@ router.post('/login', function(req, res, next) {
     })
   })(req, res, next)
 })
+
+
+router.post('/register', [
+  body('firstName', 'Please enter a valid first name.').notEmpty().bail().isAlpha().trim().escape(),
+  body('lastName', 'Please enter a valid last name.').notEmpty().bail().isAlpha().trim().escape(),
+  body('age', 'Please enter a valid age.').notEmpty().bail().isInt({min: 1, max: 125, allow_leading_zeroes: false}),
+  body('sex', 'Please enter sex.').notEmpty().bail().isAlpha().trim().escape(),
+  body('mobile', 'Please enter a valid mobile number.').notEmpty().bail().isMobilePhone("en-IN"),
+  body('email', 'Please enter a valid email.').optional({ checkFalsy: true }).isEmail().normalizeEmail(),
+  body('village', 'Please enter village.').notEmpty().bail().isAlpha().trim().escape(),
+  body('block', 'Please enter block.').notEmpty().bail().isAlpha().trim().escape(),
+  body('district', 'Please enter disctrict.').notEmpty().bail().isAlpha().trim().escape(),
+  body('state', 'Please enter state.').notEmpty().bail().isAlpha().trim().escape(),
+  body('password', 'Please enter password of atleast 5 characters.').notEmpty().bail().isLength({min: 5}).trim().escape(),
+], function(req, res, next) {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    console.log(errors)
+    return res.send({errors: errors.array()})
+  }
+  Farmer.register(new Farmer({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    age: req.body.age,
+    sex: req.body.sex,
+    mobile: req.body.mobile,
+    email: req.body.email,
+    village: req.body.village,
+    block: req.body.block,
+    district: req.body.district,
+    state: req.body.state}), req.body.password, function(err) {
+      if (err) {
+        console.log('Error while Farmer register!', err);
+        return res.send({exists: true});
+      }
+      console.log('Farmer registered!');
+      return res.send({success: 'Farmer registerd successfully.'});
+  });
+});
+
 
 module.exports = router
