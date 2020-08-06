a2a63f59bd81f97da25f3a5895ccda691ee09b0f
unknown <saprativa@gmail.com>
2020-08-06 06:12:19 +0530

    carousel

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,31 +1,62 @@
-import React, {Component} from 'react'
-import { Jumbotron, Button } from 'reactstrap'
+import React from 'react'
+// import { Jumbotron, Button } from 'reactstrap'
+import { UncontrolledCarousel } from 'reactstrap';
 
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        <Jumbotron>
-          <h1 className="display-3">Welcome to Kisaan Darshan</h1>
-          <p className="lead">
-            Farmers leading the world of agricultural ecosystem through Kisaan Darshan where all 
-            stockholders of agriculture viz. investors, markets, consumers, intellectuals &amp; 
-            professionals along with government entities interacts freely center to farmers and 
-            farming community.
-          </p>
-          <hr className="my-2" />
-          <p>
-            Kisaan Darshan enables farmers to lead the world of agricultural systems. Here all the 
-            stakeholders viz. investors, markets, consumers, intellectuals &amp; professionals along with
-             government entities interact freely with respect to farmers and farming communities.
-          </p>
-          <p className="lead">
-            <Button color="primary">Login</Button>
-          </p>
-        </Jumbotron>
-      </div>
-    )
+const items = [
+  {
+    src: './images/home/car1.png',
+    altText: 'Slide 1',
+    caption: '',
+    header: 'Kisaan Darshan',
+    key: '1'
+  },
+  {
+    src: './images/home/car2.jpg',
+    altText: 'Slide 2',
+    caption: '',
+    header: 'Kisaan Darshan',
+    key: '2'
+  },
+  {
+    src: './images/home/car3.jpg',
+    altText: 'Slide 3',
+    caption: '',
+    header: 'Kisaan Darshan',
+    key: '3'
   }
-}
+];
+
+
+const Home = () => <UncontrolledCarousel items={items} />
 
 export default Home
+
+
+// class Home extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <Jumbotron>
+//           <h1 className="display-3">Welcome to Kisaan Darshan</h1>
+//           <p className="lead">
+//             Farmers leading the world of agricultural ecosystem through Kisaan Darshan where all 
+//             stockholders of agriculture viz. investors, markets, consumers, intellectuals &amp; 
+//             professionals along with government entities interacts freely center to farmers and 
+//             farming community.
+//           </p>
+//           <hr className="my-2" />
+//           <p>
+//             Kisaan Darshan enables farmers to lead the world of agricultural systems. Here all the 
+//             stakeholders viz. investors, markets, consumers, intellectuals &amp; professionals along with
+//              government entities interact freely with respect to farmers and farming communities.
+//           </p>
+//           <p className="lead">
+//             <Button color="primary">Login</Button>
+//           </p>
+//         </Jumbotron>
+//       </div>
+//     )
+//   }
+// }
+
+// export default Home
